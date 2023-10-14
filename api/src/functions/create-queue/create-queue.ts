adbd108ae95f58c8b143177b0eacc6712719dbcc
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-10-15 04:02:22 +0800

    test(): run worker after successful add queue

--- api/src/functions/create-queue/create-queue.ts
+++ api/src/functions/create-queue/create-queue.ts
@@ -1,8 +1,10 @@
 import type { APIGatewayEvent, Context } from 'aws-lambda'
 import { logger } from 'src/lib/logger'
-import { Queue } from 'bullmq'
+import { Queue, Worker } from 'bullmq'
 import { CONFIG } from 'src/lib/constants'
 import { emailQueue } from 'src/lib/email'
+
+const axios = require('axios');
 /**
  * The handler function is your code that processes http request events.
  * You can use return and throw to send a response or error, respectively.
@@ -33,7 +35,53 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
       batch: 'Batch  1',
       origin: 'www.google.com',
     },
-  )
+  ).then(_queue => {
+    const worker = new Worker(
+      'email',
+      async (job) => {
+        const { id, data } = job
+
+        CONFIG.vercel.environment === 'production'
+          ? console.log(`Job ID: ${id} is being processed!`)
+          : logger.info(`Job ID: ${id} is being processed!`)
+
+        await emailProcess()
+      },
+      {
+        lockDuration: 30000,
+        connection: CONFIG.redis.jobQueueConnection,
+      }
+    )
+
+    worker.on('completed', (job) => {
+      CONFIG.vercel.environment === 'production'
+        ? console.log(`Job ID: ${job.id} is done!`)
+        : logger.info(`Job ID: ${job.id} is done!`)
+    })
+    worker.on('active', (job) => {
+      CONFIG.vercel.environment === 'production'
+        ? console.log(`Job ID: ${job.id} is running!`)
+        : logger.info(`Job ID: ${job.id} is running!`)
+    })
+    worker.on('error', (err) => {
+      CONFIG.vercel.environment === 'production'
+        ? console.error(err)
+        : logger.error(err)
+    })
+
+    worker.on('failed', (job, err) => {
+      CONFIG.vercel.environment === 'production'
+        ? console.error(`${job?.id} has failed with ${err.message}`)
+        : logger.error(`${job?.id} has failed with ${err.message}`)
+    })
+    worker.on('drained', () => {
+      CONFIG.vercel.environment === 'production'
+        ? console.log(`No more jobs`)
+        : logger.info(`No more jobs`)
+
+        worker.close()
+    })
+  })
 
   return {
     statusCode: 200,
@@ -43,5 +91,35 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
     body: JSON.stringify({
       data: 'createQueue function',
     }),
+  }
+}
+
+const emailProcess = async () => {
+  try {
+    const response = await axios.post('https://api.brevo.com/v3/smtp/email', {
+      sender: {
+        name: `"Atlas Admin" <${CONFIG.brevo.senderEmail}>`,
+        email: CONFIG.brevo.tempSenderEmail,
+      },
+      to: [
+        {
+          email: 'jemuel.lupo@gmail.com',
+          name: 'Dev Testing',
+        },
+      ],
+      htmlContent: 'Hello, this is testing',
+      subject: 'Testing this part',
+    }, {
+      headers: {
+        'accept': 'application/json',
+        'api-key': CONFIG.brevo.apiKey,
+      },
+    });
+
+    // Handle the response here if needed
+    console.log('Email sent:', response.data);
+  } catch (error) {
+    // Handle errors here
+    console.error('Error sending email:', error);
   }
 }
