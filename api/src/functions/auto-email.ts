3e2d41f2b9ae7d32e48c4b9303b99ab64f6df544
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-10-15 03:51:44 +0800

    test(): eject email process from auto run

--- api/src/functions/auto-email.ts
+++ api/src/functions/auto-email.ts
@@ -1,85 +1,85 @@
-import { Worker } from "bullmq"
-import { CONFIG } from "src/lib/constants"
-import { logger } from "src/lib/logger"
+// import { Worker } from "bullmq"
+// import { CONFIG } from "src/lib/constants"
+// import { logger } from "src/lib/logger"
 
-const axios = require('axios');
+// const axios = require('axios');
 
-const worker = new Worker(
-  'email',
-  async (job) => {
-    const { id } = job
+// const worker = new Worker(
+//   'email',
+//   async (job) => {
+//     const { id } = job
 
-    CONFIG.vercel.environment === 'production'
-      ? console.log(`Job ID: ${id} is being processed!`)
-      : logger.info(`Job ID: ${id} is being processed!`)
+//     CONFIG.vercel.environment === 'production'
+//       ? console.log(`Job ID: ${id} is being processed!`)
+//       : logger.info(`Job ID: ${id} is being processed!`)
 
-    await emailProcess()
-  },
-  {
-    // removeOnComplete: {
-    //   age: 1,
-    //   count: 0,
-    // },
-    lockDuration: 30000,
-    connection: CONFIG.redis.jobQueueConnection,
-  }
-)
+//     await emailProcess()
+//   },
+//   {
+//     // removeOnComplete: {
+//     //   age: 1,
+//     //   count: 0,
+//     // },
+//     lockDuration: 30000,
+//     connection: CONFIG.redis.jobQueueConnection,
+//   }
+// )
 
-worker.on('completed', (job) => {
-  CONFIG.vercel.environment === 'production'
-    ? console.log(`Job ID: ${job.id} is done!`)
-    : logger.info(`Job ID: ${job.id} is done!`)
-})
-worker.on('active', (job) => {
-  CONFIG.vercel.environment === 'production'
-    ? console.log(`Job ID: ${job.id} is running!`)
-    : logger.info(`Job ID: ${job.id} is running!`)
-})
-worker.on('error', (err) => {
-  CONFIG.vercel.environment === 'production'
-    ? console.error(err)
-    : logger.error(err)
-})
+// worker.on('completed', (job) => {
+//   CONFIG.vercel.environment === 'production'
+//     ? console.log(`Job ID: ${job.id} is done!`)
+//     : logger.info(`Job ID: ${job.id} is done!`)
+// })
+// worker.on('active', (job) => {
+//   CONFIG.vercel.environment === 'production'
+//     ? console.log(`Job ID: ${job.id} is running!`)
+//     : logger.info(`Job ID: ${job.id} is running!`)
+// })
+// worker.on('error', (err) => {
+//   CONFIG.vercel.environment === 'production'
+//     ? console.error(err)
+//     : logger.error(err)
+// })
 
-worker.on('failed', (job, err) => {
-  CONFIG.vercel.environment === 'production'
-    ? console.error(`${job?.id} has failed with ${err.message}`)
-    : logger.error(`${job?.id} has failed with ${err.message}`)
-})
-worker.on('drained', () => {
-  CONFIG.vercel.environment === 'production'
-    ? console.log(`No more jobs`)
-    : logger.info(`No more jobs`)
+// worker.on('failed', (job, err) => {
+//   CONFIG.vercel.environment === 'production'
+//     ? console.error(`${job?.id} has failed with ${err.message}`)
+//     : logger.error(`${job?.id} has failed with ${err.message}`)
+// })
+// worker.on('drained', () => {
+//   CONFIG.vercel.environment === 'production'
+//     ? console.log(`No more jobs`)
+//     : logger.info(`No more jobs`)
 
-    worker.close()
-})
+//     worker.close()
+// })
 
-const emailProcess = async () => {
-  try {
-    const response = await axios.post('https://api.brevo.com/v3/smtp/email', {
-      sender: {
-        name: `"Atlas Admin" <${CONFIG.brevo.senderEmail}>`,
-        email: CONFIG.brevo.tempSenderEmail,
-      },
-      to: [
-        {
-          email: 'jemuel.lupo@gmail.com',
-          name: 'Dev Testing',
-        },
-      ],
-      htmlContent: 'Hello, this is testing',
-      subject: 'Testing this part',
-    }, {
-      headers: {
-        'accept': 'application/json',
-        'api-key': CONFIG.brevo.apiKey,
-      },
-    });
+// export const emailProcess = async () => {
+//   try {
+//     const response = await axios.post('https://api.brevo.com/v3/smtp/email', {
+//       sender: {
+//         name: `"Atlas Admin" <${CONFIG.brevo.senderEmail}>`,
+//         email: CONFIG.brevo.tempSenderEmail,
+//       },
+//       to: [
+//         {
+//           email: 'jemuel.lupo@gmail.com',
+//           name: 'Dev Testing',
+//         },
+//       ],
+//       htmlContent: 'Hello, this is testing',
+//       subject: 'Testing this part',
+//     }, {
+//       headers: {
+//         'accept': 'application/json',
+//         'api-key': CONFIG.brevo.apiKey,
+//       },
+//     });
 
-    // Handle the response here if needed
-    console.log('Email sent:', response.data);
-  } catch (error) {
-    // Handle errors here
-    console.error('Error sending email:', error);
-  }
-};
+//     // Handle the response here if needed
+//     console.log('Email sent:', response.data);
+//   } catch (error) {
+//     // Handle errors here
+//     console.error('Error sending email:', error);
+//   }
+// };
--- api/src/functions/hello/hello.ts
+++ api/src/functions/hello/hello.ts
@@ -1,8 +1,8 @@
 import type { APIGatewayEvent, Context } from 'aws-lambda'
+import { CONFIG } from 'src/lib/constants';
 import { emailQueue } from 'src/lib/email'
 import { logger } from 'src/lib/logger'
-import { emailProcess } from '../auto-email'
-
+const axios = require('axios');
 /**
  * The handler function is your code that processes http request events.
  * You can use return and throw to send a response or error, respectively.
@@ -19,6 +19,37 @@ import { emailProcess } from '../auto-email'
  * @param { Context } context - contains information about the invocation,
  * function, and execution environment.
  */
+
+const emailProcess = async () => {
+    try {
+      const response = await axios.post('https://api.brevo.com/v3/smtp/email', {
+        sender: {
+          name: `"Atlas Admin" <${CONFIG.brevo.senderEmail}>`,
+          email: CONFIG.brevo.tempSenderEmail,
+        },
+        to: [
+          {
+            email: 'jemuel.lupo@gmail.com',
+            name: 'Dev Testing',
+          },
+        ],
+        htmlContent: 'Hello, this is testing',
+        subject: 'Testing this part',
+      }, {
+        headers: {
+          'accept': 'application/json',
+          'api-key': CONFIG.brevo.apiKey,
+        },
+      });
+
+      // Handle the response here if needed
+      console.log('Email sent:', response.data);
+    } catch (error) {
+      // Handle errors here
+      console.error('Error sending email:', error);
+    }
+  };
+
 export const handler = async (event: APIGatewayEvent, context: Context) => {
   logger.info('Invoked hello function')
 
