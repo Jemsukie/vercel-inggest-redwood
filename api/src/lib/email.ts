8396f162b69312f6c2e192c0a0bcd95262272059
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-10-15 23:19:07 +0800

    test(): remove awaits

--- api/src/lib/email.ts
+++ api/src/lib/email.ts
@@ -12,17 +12,17 @@ const emailQueue = new Queue('email', {
 emailQueue.isReady().then(async (_res) => {
   console.log('--EmailQueue is now Ready!')
 
-  await emailQueue.process(async (job, done) => {
+  emailQueue.process(async (job, done) => {
     console.log(`Job ${job.id} is now in process!`)
 
     done()
   })
 
-  await emailQueue.on('waiting', (jobId) => {
+  emailQueue.on('waiting', (jobId) => {
     console.log(`Job ${jobId} is now in waiting list!`)
   })
 
-  await emailQueue.on('active', (job) => {
+  emailQueue.on('active', (job) => {
     console.log(`Job ${job.id} is now in active!`)
   })
 })
