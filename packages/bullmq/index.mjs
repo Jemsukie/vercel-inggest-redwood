dc95762e96b2f3e6af75146b7708885e50ac1477
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-10-17 03:09:54 +0800

    test(): run worker on express listener

--- packages/bullmq/index.mjs
+++ packages/bullmq/index.mjs
@@ -6,39 +6,41 @@ import emailProcess from './utils/email.mjs'
 
 const app = express()
 
-const worker = new Worker(
-  'email',
-  async (job) => {
-    const { id, data } = job
+app.listen(() => {
+  const worker = new Worker(
+    'email',
+    async (job) => {
+      const { id, data } = job
 
-    console.log(`${new Date()} - Job ID: ${id} is being processed!`)
+      console.log(`${new Date()} - Job ID: ${id} is being processed!`)
 
-    await emailProcess({ data })
-  },
-  {
-    removeOnComplete: {
-      age: 1,
-      count: 0,
+      await emailProcess({ data })
     },
-    lockDuration: 3600000,
-    connection: CONFIG.redis.jobQueueConnection,
-  }
-)
+    {
+      removeOnComplete: {
+        age: 1,
+        count: 0,
+      },
+      lockDuration: 3600000,
+      connection: CONFIG.redis.jobQueueConnection,
+    }
+  )
 
-worker.on('completed', (job) => {
-  console.log(`${new Date()} - Job ID: ${job.id} is done!`)
-})
-worker.on('active', (job) => {
-  console.log(`${new Date()} - Job ID: ${job.id} is running!`)
-})
-worker.on('error', (err) => {
-  console.error(err)
-})
-worker.on('failed', (job, err) => {
-  console.error(`${new Date()} - ${job.id} has failed with ${err}`)
-})
-worker.on('drained', () => {
-  console.log(`${new Date()} - No more jobs`)
+  worker.on('completed', (job) => {
+    console.log(`${new Date()} - Job ID: ${job.id} is done!`)
+  })
+  worker.on('active', (job) => {
+    console.log(`${new Date()} - Job ID: ${job.id} is running!`)
+  })
+  worker.on('error', (err) => {
+    console.error(err)
+  })
+  worker.on('failed', (job, err) => {
+    console.error(`${new Date()} - ${job.id} has failed with ${err}`)
+  })
+  worker.on('drained', () => {
+    console.log(`${new Date()} - No more jobs`)
+  })
 })
 
 export default app
