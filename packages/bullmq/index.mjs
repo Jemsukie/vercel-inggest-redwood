64fe6fdec496ea08c59ffcd7a5a5f799db0929f2
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-10-17 05:11:47 +0800

    test(): add routes and cron every minute

--- packages/bullmq/index.mjs
+++ packages/bullmq/index.mjs
@@ -1,7 +1,9 @@
 import { Worker } from 'bullmq'
 import dotenv from 'dotenv'
+import express from 'express'
 
 dotenv.config()
+const app = express()
 
 const CONFIG = {
   redis: {
@@ -107,37 +109,41 @@ const compileTemplate = ({ data, params }) => {
   }
 }
 
-const worker = new Worker(
-  'email',
-  async (job) => {
-    const { id, data } = job
+app.get('/', (_req, _res) => {
+  const worker = new Worker(
+    'email',
+    async (job) => {
+      const { id, data } = job
 
-    console.log(`${new Date()} - Job ID: ${id} is being processed!`)
+      console.log(`${new Date()} - Job ID: ${id} is being processed!`)
 
-    await emailProcess({ data })
-  },
-  {
-    removeOnComplete: {
-      age: 1,
-      count: 0,
+      await emailProcess({ data })
     },
-    lockDuration: 3600000,
-    connection: CONFIG.redis.jobQueueConnection,
-  }
-)
+    {
+      removeOnComplete: {
+        age: 1,
+        count: 0,
+      },
+      lockDuration: 3600000,
+      connection: CONFIG.redis.jobQueueConnection,
+    }
+  )
 
-worker.on('completed', (job) => {
-  console.log(`${new Date()} - Job ID: ${job.id} is done!`)
+  worker.on('completed', (job) => {
+    console.log(`${new Date()} - Job ID: ${job.id} is done!`)
+  })
+  worker.on('active', (job) => {
+    console.log(`${new Date()} - Job ID: ${job.id} is running!`)
+  })
+  worker.on('error', (err) => {
+    console.error(err)
+  })
+  worker.on('failed', (job, err) => {
+    console.error(`${new Date()} - ${job.id} has failed with ${err}`)
+  })
+  worker.on('drained', () => {
+    console.log(`${new Date()} - No more jobs`)
+  })
 })
-worker.on('active', (job) => {
-  console.log(`${new Date()} - Job ID: ${job.id} is running!`)
-})
-worker.on('error', (err) => {
-  console.error(err)
-})
-worker.on('failed', (job, err) => {
-  console.error(`${new Date()} - ${job.id} has failed with ${err}`)
-})
-worker.on('drained', () => {
-  console.log(`${new Date()} - No more jobs`)
-})
+
+export default app
