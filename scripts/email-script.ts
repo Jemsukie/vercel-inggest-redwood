1cd3dd8c808139eb1a98396d8b7014705b0eaa08
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-10-17 14:06:33 +0800

    test(): run exec in background

--- scripts/email-script.ts
+++ scripts/email-script.ts
@@ -1,71 +1,43 @@
-import {CONFIG} from 'api/src/lib/constants'
+import { CONFIG } from 'api/src/lib/constants'
 import { Worker } from 'bullmq'
 
 export default async () => {
-  try{
+  try {
     const worker = new Worker(
-  'email',
-  async (job) => {
-    const { id, data } = job
+      'email',
+      async (job) => {
+        const { id, data } = job
 
-    console.log(`Job ID: ${id} is being processed!`)
-    await emailProcess()
-  },
-  {
-    // removeOnComplete: {
-    //   age: 1,
-    //   count: 0,
-    // },
-    lockDuration: 30000,
-    connection: CONFIG.redis.jobQueueConnection,
-  }
-)
-
-worker.on('completed', (job) => {
-  console.log(`Job ID: ${job.id} is done!`)
-})
-worker.on('active', (job) => {
-  console.log(`Job ID: ${job.id} is running!`)
-})
-worker.on('error', (err) => {
-  console.error(err)
-})
-
-worker.on('failed', (job, err) => {
-  console.error(`${job?.id} has failed with ${err.message}`)
-})
-worker.on('drained', () => {
-  console.log(`No more jobs`)
-
-    worker.close()
-})
-
-const emailProcess = async() => {
-  await fetch('https://api.brevo.com/v3/smtp/email', {
-    // TODO, merge options.headers if exist
-    headers: {
-      accept: 'application/json',
-      'api-key': CONFIG.brevo.apiKey,
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      sender: {
-        name: `"Atlas Admin" <${CONFIG.brevo.senderEmail}>`,
-        email: CONFIG.brevo.tempSenderEmail,
+        console.log(`Job ID: ${id} is being processed!`)
+        console.log('--this is data', data)
       },
-      to: [
-        {
-          email: 'jemuel.lupo@gmail.com',
-          name: 'Dev Testing',
-        },
-      ],
-      htmlContent: 'Hello this is testing',
-      subject: 'Testing this part',
-    }),
-  })
-}
+      {
+        // removeOnComplete: {
+        //   age: 1,
+        //   count: 0,
+        // },
+        lockDuration: 30000,
+        connection: CONFIG.redis.jobQueueConnection,
+      }
+    )
 
-  } catch(error){
+    worker.on('completed', (job) => {
+      console.log(`Job ID: ${job.id} is done!`)
+    })
+    worker.on('active', (job) => {
+      console.log(`Job ID: ${job.id} is running!`)
+    })
+    worker.on('error', (err) => {
+      console.error(err)
+    })
+
+    worker.on('failed', (job, err) => {
+      console.error(`${job?.id} has failed with ${err.message}`)
+    })
+    worker.on('drained', () => {
+      console.log(`No more jobs`)
+    })
+  } catch (error) {
     console.error(error)
   }
 }
