9c5d1c8887ad935bc8dab2c91da60cb1610bd14a
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-10-17 11:03:51 +0800

    test(0: run only console.log

--- packages/bullmq/index.mjs
+++ packages/bullmq/index.mjs
@@ -24,88 +24,88 @@ const CONFIG = {
   },
 }
 
-const _emailProcess = ({
-  data: { tribeId, email, name, tempPassword, batch, origin, template },
-}) => {
-  const params = {
-    tribeId,
-    email,
-    name,
-    batch,
-    tempPassword,
-    loginLink: `${origin}/login?extraData=${btoa(JSON.stringify({ email }))}`,
-    forgotPasswordLink: `${origin}/set-new-password?extraData=${btoa(
-      JSON.stringify({
-        email,
-        password: tempPassword,
-      })
-    )}`,
-  }
+// const _emailProcess = ({
+//   data: { tribeId, email, name, tempPassword, batch, origin, template },
+// }) => {
+//   const params = {
+//     tribeId,
+//     email,
+//     name,
+//     batch,
+//     tempPassword,
+//     loginLink: `${origin}/login?extraData=${btoa(JSON.stringify({ email }))}`,
+//     forgotPasswordLink: `${origin}/set-new-password?extraData=${btoa(
+//       JSON.stringify({
+//         email,
+//         password: tempPassword,
+//       })
+//     )}`,
+//   }
 
-  return sendEmail({
-    to: email,
-    template,
-    params,
-  })
-}
+//   return sendEmail({
+//     to: email,
+//     template,
+//     params,
+//   })
+// }
 
-const sendEmail = async ({ to, template, params }) => {
-  const data = JSON.parse(template)
-  return await fetch('https://api.brevo.com/v3/smtp/email', {
-    // TODO, merge options.headers if exist
-    headers: {
-      accept: 'application/json',
-      'api-key': CONFIG.brevo.apiKey,
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      sender: {
-        name: `"Atlas Admin" <${CONFIG.brevo.senderEmail}>`,
-        email: CONFIG.brevo.tempSenderEmail,
-      },
-      to: [
-        {
-          email: to,
-          name: params.name,
-        },
-      ],
-      ...compileTemplate({ data, params }),
-    }),
-  })
-    .then((result) => {
-      console.log('Email Sent!')
-      return result
-    })
-    .catch((err) => console.error(err))
-}
+// const sendEmail = async ({ to, template, params }) => {
+//   const data = JSON.parse(template)
+//   return await fetch('https://api.brevo.com/v3/smtp/email', {
+//     // TODO, merge options.headers if exist
+//     headers: {
+//       accept: 'application/json',
+//       'api-key': CONFIG.brevo.apiKey,
+//     },
+//     method: 'POST',
+//     body: JSON.stringify({
+//       sender: {
+//         name: `"Atlas Admin" <${CONFIG.brevo.senderEmail}>`,
+//         email: CONFIG.brevo.tempSenderEmail,
+//       },
+//       to: [
+//         {
+//           email: to,
+//           name: params.name,
+//         },
+//       ],
+//       ...compileTemplate({ data, params }),
+//     }),
+//   })
+//     .then((result) => {
+//       console.log('Email Sent!')
+//       return result
+//     })
+//     .catch((err) => console.error(err))
+// }
 
-const compileTemplate = ({ data, params }) => {
-  let HTML_CONTENT = data.htmlContent
-  let SUBJECT = data.subject
+// const compileTemplate = ({ data, params }) => {
+//   let HTML_CONTENT = data.htmlContent
+//   let SUBJECT = data.subject
 
-  const paramArray = Object.entries(params).map(([key, value]) => {
-    return {
-      toReplace: `{{params.${key}}}`,
-      toPut: value,
-    }
-  })
+//   const paramArray = Object.entries(params).map(([key, value]) => {
+//     return {
+//       toReplace: `{{params.${key}}}`,
+//       toPut: value,
+//     }
+//   })
 
-  // Iterate through the paramArray and replace placeholders in the HTML
-  paramArray.forEach((param) => {
-    const { toReplace, toPut } = param
-    const regex = new RegExp(
-      toReplace.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'),
-      'g'
-    )
-    HTML_CONTENT = HTML_CONTENT.replace(regex, toPut)
-    SUBJECT = SUBJECT.replace(regex, toPut)
-  })
+//   // Iterate through the paramArray and replace placeholders in the HTML
+//   paramArray.forEach((param) => {
+//     const { toReplace, toPut } = param
+//     const regex = new RegExp(
+//       toReplace.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'),
+//       'g'
+//     )
+//     HTML_CONTENT = HTML_CONTENT.replace(regex, toPut)
+//     SUBJECT = SUBJECT.replace(regex, toPut)
+//   })
 
-  return {
-    htmlContent: HTML_CONTENT,
-    subject: SUBJECT,
-  }
-}
+//   return {
+//     htmlContent: HTML_CONTENT,
+//     subject: SUBJECT,
+//   }
+// }
 
 const worker = new Worker(
   'email',
@@ -139,6 +139,6 @@ worker.on('error', (err) => {
 worker.on('failed', (job, err) => {
   console.error(`${new Date()} - ${job.id} has failed with ${err}`)
 })
-worker.on('drained', () => {
-  console.log(`${new Date()} - No more jobs`)
-})
+// worker.on('drained', () => {
+//   console.log(`${new Date()} - No more jobs`)
+// })
